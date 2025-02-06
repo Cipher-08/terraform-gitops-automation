eb8f28ef022a3049f19c42927887d32d42431efc
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2025-02-06 16:34:05 +0530

    Update backend.tf

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-storage"
-    key    = "gitops-demo/terraform.tfstate"
-    region = "ap-southeast-2"
-    encrypt = true
+    bucket         = "terraform-state-storage-1"
+    key            = "gitops-demo/terraform.tfstate"
+    region         = "ap-southeast-2"
+    access_key     = "AKIAYLZZJWQA6N6WOUET"
+    secret_key     = "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"
+    encrypt        = true
   }
 }
