fc40fb5f08fee89b158873d7a47f8040b7bccade
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2025-02-06 15:24:45 +0530

    Update backend.tf

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-state-storage"
     key    = "gitops-demo/terraform.tfstate"
-    region = "ap-southeast-2"   # ✅ Make sure this matches your S3 bucket's region
+    region = "ap-southeast-2"   # ✅ Make sure this matces your S3 bucket's region
     encrypt = true
   }
 }
